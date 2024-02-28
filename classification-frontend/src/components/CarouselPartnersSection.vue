7bd0a193ab96ba30d3fa3c775b981771e1b00080
AgustinVissani <vissani1997@gmail.com>
2024-02-28 13:09:32 +0100

    Partners section and carousel

---
+++ classification-frontend/src/components/CarouselPartnersSection.vue
@@ -0,0 +1,119 @@
+<template>
+  <q-carousel
+    swipeable
+    animated
+    arrows
+    v-model="slide"
+    infinite
+    control-type="regular"
+    control-color="grey"
+    control-text-color="blue"
+    q-mt-xl
+  >
+    <q-carousel-slide
+      v-lazy-image
+      v-lazy-image-loaded
+      :name="0"
+      class="relative-position scroll flex-center"
+    >
+      <div class="row justify-around full-width q-px-sm text-center">
+        <div class="col">
+          <img
+            src="/equest.png"
+            v-lazy-image
+            v-lazy-image-loaded
+            class="sponsor-logo"
+          />
+        </div>
+      </div>
+      <div class="row justify-around full-width q-px-sm text-center">
+        <div class="col">
+          <img
+            src="/recruitly.png"
+            v-lazy-image
+            v-lazy-image-loaded
+            class="sponsor-logo"
+          />
+        </div>
+      </div>
+      <div class="row justify-around full-width q-px-sm text-center">
+        <div class="col">
+          <img
+            src="/lever.png"
+            v-lazy-image
+            v-lazy-image-loaded
+            class="sponsor-logo"
+          />
+        </div>
+      </div>
+    </q-carousel-slide>
+    <q-carousel-slide
+      v-lazy-image
+      v-lazy-image-loaded
+      :name="1"
+      class="relative-position scroll flex-center"
+    >
+      <div class="row justify-around full-width q-px-sm text-center">
+        <div class="col">
+          <img
+            src="/broadbean.svg"
+            v-lazy-image
+            v-lazy-image-loaded
+            class="sponsor-logo"
+          />
+        </div>
+      </div>
+      <div class="row justify-around full-width q-px-sm text-center">
+        <div class="col">
+          <img
+            src="/hrlink.svg"
+            v-lazy-image
+            v-lazy-image-loaded
+            class="sponsor-logo"
+          />
+        </div>
+      </div>
+      <div class="row justify-around full-width q-px-sm text-center">
+        <div class="col">
+          <img
+            src="/logicmelon.svg"
+            v-lazy-image
+            v-lazy-image-loaded
+            class="sponsor-logo"
+          />
+        </div>
+      </div>
+    </q-carousel-slide>
+  </q-carousel>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+
+export default {
+  setup() {
+    const slide = ref(0);
+    const interval = 3000;
+
+    onMounted(() => {
+      setInterval(() => {
+        slide.value = (slide.value + 1) % 2;
+      }, interval);
+    });
+
+    return {
+      slide,
+    };
+  },
+};
+</script>
+
+<style>
+.q-carousel-slide {
+  transition: transform 0.3s ease;
+}
+
+.q-carousel-inner {
+  transform: translateX(-100%);
+}
+</style>
---
+++ classification-frontend/src/components/PartnersSection.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="no-padding-top bg-white">
+    <div class="page-container layout-padding">
+      <div id="partners" class="row partners">
+        <div class="col-12" style="padding-top: 15px">
+          <h2 class="title text-center">
+            <strong>ATS Partners &amp; API Solutions</strong>
+          </h2>
+        </div>
+      </div>
+      <div class="row q-pb-lg">
+        <div class="col-12">
+          <CarouselPartnersSection />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CarouselPartnersSection from "./CarouselPartnersSection.vue";
+
+export default {
+  components: {
+    CarouselPartnersSection,
+  },
+};
+</script>
+
+<style>
+.partners .title {
+  color: #0c3866;
+  font-size: 22px;
+}
+</style>
