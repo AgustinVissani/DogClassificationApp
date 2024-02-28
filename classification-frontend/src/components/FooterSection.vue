09568a03083c7351e69849559c18b90eba6d35d0
AgustinVissani <vissani1997@gmail.com>
2024-02-28 13:10:12 +0100

    Footel section

---
+++ classification-frontend/src/components/FooterSection.vue
@@ -0,0 +1,330 @@
+<template>
+  <footer data-v-7d430532="" class="footer q-pa-md">
+    <div data-v-7d430532="" class="footer-content layout-padding">
+      <hr data-v-7d430532="" color="#0c3866" />
+      <div data-v-7d430532="" class="row q-mt-lg" style="margin-bottom: 2.4em">
+        <div data-v-7d430532="" class="col-md-4">
+          <a data-v-7d430532="" href="/" class="router-link-active"
+            ><img
+              data-v-7d430532=""
+              src="/logo-elj-no-bg.png"
+              alt="Europe Language Jobs"
+              style="height: 100px"
+          /></a>
+          <p
+            data-v-7d430532=""
+            class="text-secondary q-mt-md q-mb-lg"
+            style="font-size: 14px"
+          >
+            <b data-v-7d430532="">
+              Europe Language Jobs - the job <br data-v-7d430532="" />
+              board for expat jobs abroad
+            </b>
+          </p>
+          <p data-v-7d430532="" class="text-primary" style="font-size: 14px">
+            We help expats find jobs in Europe using their
+            <br data-v-7d430532="" />
+            native language and gain international experience
+            <br data-v-7d430532="" />
+            by working in a foreign country.
+          </p>
+        </div>
+        <div data-v-7d430532="" class="col-md-3 q-mt-md">
+          <p data-v-7d430532="" class="text-primary">
+            <b data-v-7d430532="">FOR JOB SEEKERS</b>
+          </p>
+          <p data-v-7d430532="">
+            <a
+              data-v-7d430532=""
+              href="/jobs"
+              class="link text-14"
+              style="color: #858586; text-decoration: none;"
+            >
+              Find a job
+            </a>
+          </p>
+          <p data-v-7d430532="">
+            <a
+              data-v-7d430532=""
+              href="/candidates"
+              class="link text-14"
+              style="color: #858586; text-decoration: none;"
+            >
+              Create an account
+            </a>
+          </p>
+          <p data-v-7d430532=""><!----></p>
+          <p data-v-7d430532="">
+            <a
+              data-v-7d430532=""
+              href="/blog"
+              class="link text-14"
+              style="color: #858586; text-decoration: none;"
+            >
+              Career advice
+            </a>
+          </p>
+          <p data-v-7d430532="" class="text-primary q-pt-md">
+            <b data-v-7d430532="">STAY CONNECTED</b>
+          </p>
+          <div data-v-7d430532="" class="row">
+            <div data-v-7d430532="">
+              <a
+                data-v-7d430532=""
+                href="https://www.linkedin.com/company/europe-language-jobs/"
+                target="_blank"
+                class="q-mr-md"
+                ><i
+                  data-v-7d430532=""
+                  aria-hidden="true"
+                  class="q-icon cursor-pointer social-icon fab fa-linkedin-in text-white"
+                >
+                </i
+              ></a>
+            </div>
+            <div data-v-7d430532="">
+              <a
+                data-v-7d430532=""
+                href="https://www.facebook.com/EuropeLanguageJobs"
+                target="_blank"
+                class="q-mr-md"
+                ><i
+                  data-v-7d430532=""
+                  aria-hidden="true"
+                  class="q-icon cursor-pointer social-icon fab fa-facebook-f text-white"
+                >
+                </i
+              ></a>
+            </div>
+            <a
+              data-v-7d430532=""
+              href="https://www.tiktok.com/@europelanguagejobs"
+              target="_blank"
+              class="q-mr-md"
+              ><img
+                data-v-7d430532=""
+                src="/icons/tik-tok.png"
+                alt="tiktok"
+                class="cursor-pointer social-icon"
+                style="padding: 0.4em"
+            /></a>
+            <div data-v-7d430532="">
+              <a
+                data-v-7d430532=""
+                href="https://www.youtube.com/c/Europelanguagejobs"
+                target="_blank"
+                class="q-mr-md"
+                ><i
+                  data-v-7d430532=""
+                  aria-hidden="true"
+                  class="q-icon cursor-pointer social-icon fab fa-youtube text-white"
+                >
+                </i
+              ></a>
+            </div>
+            <div data-v-7d430532="">
+              <a
+                data-v-7d430532=""
+                href="https://twitter.com/intent/follow?source=followbutton&amp;variant=1.0&amp;screen_name=europelangjobs"
+                target="_blank"
+                class="q-mr-md"
+                ><i
+                  data-v-7d430532=""
+                  aria-hidden="true"
+                  class="q-icon cursor-pointer social-icon fab fa-twitter text-white"
+                >
+                </i
+              ></a>
+            </div>
+            <a
+              data-v-7d430532=""
+              href="https://discord.gg/UNKxY7rJNz"
+              target="_blank"
+              class="q-mr-md"
+              ><img
+                data-v-7d430532=""
+                src="/icons/discord_icono.png"
+                alt="discord"
+                class="cursor-pointer"
+                style="height: 1.9em; width: 1.9em"
+            /></a>
+          </div>
+        </div>
+        <div data-v-7d430532="" class="col-md-2 q-mt-md">
+          <p data-v-7d430532="" class="text-primary">
+            <b data-v-7d430532="">FOR EMPLOYERS</b>
+          </p>
+          <p data-v-7d430532="">
+            <a
+              data-v-7d430532=""
+              href="/companies"
+              class="link text-14 router-link-exact-active router-link-active"
+              style="color: #858586; text-decoration: none;"
+            >
+              Post a job
+            </a>
+          </p>
+          <p data-v-7d430532=""><!----></p>
+          <p data-v-7d430532=""><!----></p>
+          <p data-v-7d430532="">
+            <a
+              data-v-7d430532=""
+              href="/companies/registry"
+              class="link text-14"
+              style="color: #858586; text-decoration: none;"
+            >
+              Create an account
+            </a>
+          </p>
+          <p data-v-7d430532="">
+            <a
+              data-v-7d430532=""
+              href="/companies/our-services"
+              class="link text-14"
+              style="color: #858586; text-decoration: none;"
+            >
+              Hiring solutions
+            </a>
+          </p>
+          <p data-v-7d430532=""><!----></p>
+          <p data-v-7d430532=""><!----></p>
+          <div data-v-7d430532="" class="row">
+            <p data-v-7d430532="" class="text-primary q-pt-md">
+              <b data-v-7d430532="">DOWNLOAD THE APP</b>
+            </p>
+            <div data-v-7d430532="" class="button-row">
+              <a
+                data-v-7d430532=""
+                href="https://www.europelanguagejobs.com/sl/goplaces-android?utm_source=goplaces&amp;utm_medium=banner-footer-desktop&amp;utm_campaign=android&amp;internal_camp=banner-footer"
+                target="blank"
+                ><img
+                  data-v-7d430532=""
+                  src="https://static.europelanguagejobs.com/contents/images/go-places-application/google-white-button.svg"
+                  class="google-white-button" /></a
+              ><a
+                data-v-7d430532=""
+                href="https://www.europelanguagejobs.com/sl/goplaces-ios?utm_source=goplaces&amp;utm_medium=banner-footer-desktop&amp;utm_campaign=ios&amp;internal_camp=banner-footer"
+                target="blank"
+                ><img
+                  data-v-7d430532=""
+                  src="https://static.europelanguagejobs.com/contents/images/go-places-application/apple-white-button.svg"
+                  class="apple-white-button"
+              /></a>
+            </div>
+          </div>
+        </div>
+        <div data-v-7d430532="" class="col-md-1"></div>
+        <div data-v-7d430532="" class="col-md-2 q-mt-md">
+          <p data-v-7d430532="" class="text-primary">
+            <b data-v-7d430532="">EUROPE LANGUAGE JOBS</b>
+          </p>
+          <p data-v-7d430532="">
+            <a
+              data-v-7d430532=""
+              href="/about-us"
+              class="link text-14"
+              style="color: #858586; text-decoration: none;"
+            >
+              About us
+            </a>
+          </p>
+          <p data-v-7d430532="">
+            <a
+              data-v-7d430532=""
+              href="/eulangjobs/faq"
+              class="link text-14"
+              style="color: #858586; text-decoration: none;"
+            >
+              FAQ
+            </a>
+          </p>
+          <p data-v-7d430532=""><!----></p>
+          <p data-v-7d430532="">
+            <a
+              data-v-7d430532=""
+              href="/eulangjobs/legal-conditions"
+              class="link text-14"
+              style="color: #858586; text-decoration: none;"
+            >
+              Legal conditions
+            </a>
+          </p>
+          <p data-v-7d430532=""><!----></p>
+          <p data-v-7d430532=""><!----></p>
+          <p data-v-7d430532="">
+            <a
+              data-v-7d430532=""
+              href="/eulangjobs/cookies-policy"
+              class="link text-14"
+              style="color: #858586; text-decoration: none;"
+            >
+              Cookies policy
+            </a>
+          </p>
+          <p data-v-7d430532="">
+            <a
+              data-v-7d430532=""
+              href="/eulangjobs/contact"
+              class="link text-14"
+              style="color: #858586; text-decoration: none;"
+            >
+              Contact us
+            </a>
+          </p>
+          <p data-v-7d430532=""><!----></p>
+        </div>
+      </div>
+      <hr data-v-7d430532="" color="#0c3866" />
+      <div data-v-7d430532="" class="col-xs-12">
+        <span data-v-7d430532="" class="q-caption"
+          ><span data-v-7d430532="" class="text-italic">Copyright</span> © 2023
+          - Europe Language Jobs. All rights reserved.
+        </span>
+      </div>
+    </div>
+  </footer>
+</template>
+
+
+<style>
+.social-icon {
+    font-size: 16px;
+    border-radius: 100%;
+    height: 1.9em;
+    width: 1.9em;
+    background: #007cc0 !important;
+}
+
+.footer {
+    background: #ebeff3;
+    color: #858586;
+}
+.footer-content {
+    max-width: 1400px;
+    width: 100%;
+    margin: 0 auto;
+    padding-top: 15px !important;
+    padding-bottom: 15px !important;
+}
+.button-row{
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: horizontal;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: row;
+    flex-direction: row;
+    gap: 15px;
+}
+.apple-white-button {
+    height: 2.5em;
+}
+.google-white-button {
+    height: 2.5em;
+}
+b {
+    font-weight: bold;
+    color: #0c3866
+}
+
+</style>
