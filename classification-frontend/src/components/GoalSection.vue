55220847b9af67f9e524bb9136482c7c90f5916a
AgustinVissani <vissani1997@gmail.com>
2024-02-28 13:09:47 +0100

    Goal section

---
+++ classification-frontend/src/components/GoalSection.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    data-v-0699b5f8=""
+    class="q-layout-page-container q-layout-transition no-padding-top bg-blue"
+    style="padding-top: 53px"
+  >
+    <div data-v-0699b5f8="" class="page-container layout-padding">
+      <div data-v-0699b5f8="" class="row footer-caption">
+        <div data-v-0699b5f8="" class="col-12 text-center">
+          <span data-v-0699b5f8="">Together, your hires, our goal!</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.page-container {
+  max-width: 1400px;
+  margin: 0 auto;
+}
+.footer-caption {
+  padding: 50px 0;
+}
+.footer-caption div span {
+  color: #fff;
+  font-size: 32px;
+}
+</style>
